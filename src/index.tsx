af693fbaad661c5c5974a5ea537006ef2206884f
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-12-04 03:09:37 +0200

    config

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+
+import { App } from 'src/App.tsx';
+
+import './scss/styles.scss';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
