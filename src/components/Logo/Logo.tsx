291122f8473479fff67cb2a364311c93ce6d935c
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-12-06 11:09:45 +0200

    logo update

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,11 @@
-import { home } from "src/data/links.ts";
-import data from "src/data/header.json";
+import { home } from 'src/data/links.ts';
+import data from 'src/data/header.json';
+import logo from 'src/images/logo.png';
 
-const { aria, img } = data.logo;
-const { src, alt } = img;
+const { aria, alt } = data.logo;
 
 export const Logo = () => (
   <a href={home} aria-label={aria}>
-    <img src={src} alt={alt} width="104" height="26" />
+    <img src={logo} alt={alt} width="104" height="26" />
   </a>
 );
